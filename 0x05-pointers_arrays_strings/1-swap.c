6d40f98a1947c06f9d071aafcd2223de52679698
Luladey74 <hahutiye@gmail.com>
2022-12-19 01:21:20 -0800

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * swap_int - swap variable values
+ * @a: int a
+ * @b: int b
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+*a += *b;
+*b = *a - *b;
+*a = *a - *b;
+}
