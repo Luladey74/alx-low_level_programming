30c6f7c560d7d30d845a6597858cd723dfc149a2
Luladey74 <hahutiye@gmail.com>
2022-12-22 00:37:54 -0800

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,10 +6,10 @@
  */
 char *leet(char *c)
 {
-  char *cp = c;
-  char key[] = {'A', 'E', 'O', 'T', 'L'};
-  int value[] = {4, 3, 0, 7, 1};
-  unsigned int i;
+char *cp = c;
+char key[] = {'A', 'E', 'O', 'T', 'L'};
+int value[] = {4, 3, 0, 7, 1};
+unsigned int i;
 
 while (*c)
 {
